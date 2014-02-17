34abaf85c03bb67469259d1514dc5c790ba537f4
unknown <david.cuellas@aureatic.com>
2014-02-17 20:23:41 +0100

    version 2 lunes 17-02-2014

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php 
+  $a="David";
+  echo " Hello World! $a";
+  echo ' Hello World! $a';
+  echo ' Hello World! '.$a;
+ 
+  print " Hello World! ".$a;
+  print (" Hello World! ".$a);
+ //comentario de linea
+ 
+ /* comentario de parrafo
+  * 
+  * 
+  */
+ 
+  /**
+    esto es para otro tipo de comentario DOCUMENTACION
+    para comenario con el IDE del zend studio, eclipse etc
+    esto es para indicar documentación que interpreta el DIE
+    para PHP es un comenario
+    
+   
+   */
+?>
+
+
